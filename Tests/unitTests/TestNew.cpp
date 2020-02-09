d5139909c69b9b2b7a32f6cbae6f44fa743bcdcb
rushikeshnakhate <rushikeshnakhate@gmail.com>
2020-02-09 22:26:52 +0800

    #7 unittest

---
+++ Tests/unitTests/TestNew.cpp
@@ -0,0 +1,22 @@
+/*#include "../libs/googletest/include/gtest/gtest.h"
+
+TEST("when_new_order_then_order_is_in_cache", eq_test){
+
+}
+
+TEST("when_sell_new_order_then_state_LIVE", eq_test){
+
+}
+
+TEST("when_sell_new_order_then_state_LIVE", eq_test){
+
+}
+
+TEST("when_best_bid_new_order_then_state_ALLOC", eq_test){
+
+}
+
+TEST("when_best_offer_new_order_then_state_ALLOC", eq_test){
+
+}
+ */
